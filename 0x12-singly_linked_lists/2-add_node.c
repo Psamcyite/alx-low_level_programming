0c942d01c08215186574ff0b76fa3f9f0fa54326
Psamcyite <amojoayokunle@gmail.com>
2023-01-27 19:45:46 -0800

    Singly linked committed

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning
+ * of a list_t list
+ *
+ * @head: the head of the linked list
+ * @str: the string to be stored in the list
+ *
+ * Return: the address of the head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	size_t n;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		;
+
+	new_node->len = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
